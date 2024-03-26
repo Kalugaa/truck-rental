264207fe776abd4e76c6b518c25e8b02376b4422
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2024-03-26 19:57:27 +0100

    Update index.jsx

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -7,7 +7,7 @@ import { H1, HomeLink, MessageContainer } from './Favorites.styled'
 
 const Favorites = () => {
     const favorites = useSelector(getFavorites)
-    console.log(favorites);
+
     return (<>
         <ul style={{ display: "flex", flexDirection: 'column', alignItems: 'center' }}>
             {favorites.length > 0 ? favorites.map((camper) => <Camper key={camper._id} camper={camper} />) : <MessageContainer  ><H1>You don`t have favorites campers yet. You can add camper to favorite on <HomeLink to='/catalog'>Catalog page</HomeLink></H1></MessageContainer>}
